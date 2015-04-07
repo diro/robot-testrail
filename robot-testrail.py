27846e557819fb5a1ee7d75fa0cb244fcaca2f23
diro <diro.fan@gmail.com>
2015-04-07 17:45:45 +0800

    1.Fix the parsing bug. 2.Parsing the robot framework test case folder recursively

--- robot-testrail.py
+++ robot-testrail.py
@@ -2,6 +2,9 @@ from testrail import *
 from pprint import *
 import sys,getopt
 import xml.dom.minidom
+import ssl
+import os
+import fnmatch
 
 def parse_uat_result(filename):
 	cidstatus = {}
@@ -18,20 +21,23 @@ def parse_uat_result(filename):
 	return cidstatus
 
 def main():
+
+	ssl._create_default_https_context = ssl._create_unverified_context
 	resultxml = "robot_result.xml"
 	projectid = 0
 	user = "admin"
 	pwd = ""
-	testrail_url = "https://YOURIP/testrail/"
+	folder = ""
+	testrail_url = "https://172.18.51.100/testrail/"
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "",["pid=", "xml=", "user=", "pwd=", "testrail="])
+		opts, args = getopt.getopt(sys.argv[1:], "",["pid=", "folder=", "user=", "pwd=", "testrail="])
 	except getopt.GetoptError as err:
 		# print help information and exit:
 		print str(err) # will print something like "option -a not recognized"
 		sys.exit(2)
 	for o, a in opts:
-		if o == "--xml":
-			resultxml = a;
+		if o == "--folder":
+			folder = a;
 		elif o == "--pid":
 			projectid = a;
 		elif o == "--user":
@@ -42,20 +48,26 @@ def main():
 			testrail_url = a;
 		else:
 			assert False, "Unknown option" + o
-	
-	cidstatus = parse_uat_result(resultxml)
-	print "Project ID:" + projectid + " TestRail URL:" + testrail_url
+		
+	print "User:" + user + ":" + pwd
+	print "Project ID:" + str(projectid) + " TestRail URL:" + testrail_url
 	client = APIClient(testrail_url)
 	client.user = user
 	client.password = pwd
 
 	run = client.send_post('add_run/' + str(projectid), 
-		{"suite_id": 1,"name": "This is a new test run","assignedto_id": 1,"include_all": "1"})
+		{"suite_id": 1,"name": "This is a new test run","assignedto_id": 1,"include_all": 	"1"})
 
-	for cid in iter(cidstatus):
-		print "Updating test result for case:" + str(cid)
-		resp = client.send_post('add_result_for_case/' + str(run['id']) + '/' + str(cid),{"status_id":cidstatus[cid]})
+	for root, dirs, files in os.walk(folder):
+		for file in files:
+			if not fnmatch.fnmatch(file, '*_output.xml'):
+				continue
+			cidstatus = parse_uat_result(file)		
+			for cid in iter(cidstatus):
+				print "Updating test result for case:" + str(cid)
+				resp = client.send_post('add_result_for_case/' + str(run['id']) + '/' + str(cid),{"status_id":cidstatus[cid]})
 		#pprint(resp)
 	print "Done!"
 
 main();
+
